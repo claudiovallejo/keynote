1806b50c4224047048b48396709eac29b3e3be87
Claudio <hola@claudiovallejo.mx>
2017-03-10 20:04:32 -0600

    Cleared comments; added directory index; created slide proxy

--- config.rb
+++ config.rb
@@ -9,36 +9,29 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
+# Slides default layout
+page "/slide.html", layout: :main
 
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
+# Slide Proxies
+data.slides.each do |id, slide|
+  proxy "/#{slide.title.parameterize}/index.html", "/slide.html", :locals => { :slide => slide }, :ignore => true
+end
 
 # General configuration
+
+# Activate directory index
+activate :directory_indexes
 
 # Reload the browser automatically whenever files change
 configure :development do
   activate :livereload
 end
 
-###
-# Helpers
-###
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
 # Build-specific configuration
 configure :build do
   # Minify CSS on build
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 end
