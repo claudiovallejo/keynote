d3e756c55908b63dfe4dcb55edb80e8cadb1fc0e
Claudio <hola@claudiovallejo.mx>
2017-03-10 20:05:16 -0600

    Created loading animation script.

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,15 @@
-// This is where it all goes :)
+//
+const $content = document.getElementsByClassName('js-content');
+//
+$content && window.addEventListener('load', function(){
+  for (var c = 0; c < $content.length; c++) {
+    toggle($content[c], "o0", "o1");
+    toggle($content[c], 'transform-tY0', 'transform-tYm4');
+    $content[c].setAttribute('style', 'transition-delay: ' + ((c+1)*0.15) + 's;');
+  }
+});
+//
+function toggle($element, classA, classB) {
+  $element.classList.toggle(classA);
+  $element.classList.toggle(classB);
+}
